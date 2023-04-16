fe02cd3bce7427b6b9cc259fd6ac39690fe38067
mehmed <mehmed2kw@gmail.com>
2023-04-16 17:41:36 +0300

    added new sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import heroImage from "../assets/images/hero.jpg";
-import ourWorkImage from "../assets/images/ourwork.jpg";
+import duckImage from "../assets/images/duck.jpg";
 import cardImage1 from "../assets/images/habitat.jpg";
 import cardImage2 from "../assets/images/animal.jpg";
 import cardImage3 from "../assets/images/events.jpg";
+import membershipImage from "../assets/images/membership.jpg";
 import { Link } from 'react-router-dom';
 
 const Home = () => {
@@ -84,6 +85,88 @@ const Home = () => {
             </div>
           </div>
         </div>
+      </section>
+
+      <section className="section-our-work" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${duckImage})` }}>
+        <div className="section-our-work__box">
+          <div className="section-our-work__title">
+            <h2>Our Work</h2>
+          </div>
+
+          <div className="section-our-work__paragraph">
+            <p>We're proud of our successfull history saving animals, from birds to snow leopards. Learn about some of our current work.</p>
+          </div>
+
+          <div className="section-our-work__actions">
+            <button className="btn btn--white">Learn More</button>
+          </div>
+        </div>
+      </section>
+
+      <section className="section-opening-times">
+        <div className="shell">
+          <div className="section-opening-times__inner">
+            <div className="section-opening-times__title">
+              <h2>Opening Times</h2>
+            </div>
+
+            <table>
+              <tr>
+                <th>Dates</th>
+                <th>Opening</th>
+                <th>Closing</th>
+              </tr>
+
+              <tr>
+                <td>31 October 2022 - 10 February 2023</td>
+                <td>10:00</td>
+                <td>16:00</td>
+              </tr>
+
+              <tr>
+                <td>11 February 2023 - 25 March 2023</td>
+                <td>10:00</td>
+                <td>17:00</td>
+              </tr>
+
+              <tr>
+                <td>26 March 2023 - 3 September 2023</td>
+                <td>10:00</td>
+                <td>18:00</td>
+              </tr>
+
+              <tr>
+                <td>4 September 2023 - 29 October 2023</td>
+                <td>10:00</td>
+                <td>17:00</td>
+              </tr>
+
+              <tr>
+                <td>30 October 2023 - 9 February 2024</td>
+                <td>10:00</td>
+                <td>16:00</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </section>
+
+
+      <section className="section-membership" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${membershipImage})` }}>
+          <div className="section-membership__box">
+            <div className="section-membership__title">
+              <h2>Become a Member</h2>
+            </div>
+
+            <div className="section-membership__paragraph">
+              <p>Join us on our mission to protect wildlife and inspire the next generation
+                 of zoologists and conservationists. Choose the membership that's right for you.</p>
+            </div>
+
+            <div className="section-membership__actions">
+              <button className="btn btn--white">Join Now</button>
+            </div>
+          </div>
       </section>
     </>
   )
